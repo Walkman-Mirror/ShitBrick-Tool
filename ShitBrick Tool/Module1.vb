4e8f4f71aea70035ac058d4f7aaa04eafc898b44
Tristan B. Kildaire <tristankildaire@gmail.com>
2013-09-01 22:50:27 +0200

    Initial release and probably the last release.
    Signed-off-by: Tristan B. Kildaire <tristankildaire@gmail.com>

---
+++ ShitBrick Tool/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Title = (My.Application.Info.ProductName & " v" & My.Application.Info.Version.ToString)
+        Console.WriteLine("################################################################################")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("                                 " & My.Application.Info.ProductName)
+        Console.WriteLine("                                    v" & My.Application.Info.Version.ToString)
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine(" Welcome to the ShitBrick tool. This utility is used to download and install a")
+        Console.WriteLine("   certain dynamic link library that most of Deavmi's applications use, but")
+        Console.WriteLine("    this dll is referenced in a different location and causes a bug, which")
+        Console.WriteLine("         will be fixed soon, this is a temporary tool to fix that bug.")
+        Console.WriteLine()
+        Console.WriteLine("          Read more about this topic: http://bit.ly/shitbrick-utility")
+        Console.WriteLine()
+        Console.WriteLine("          Registered under the GPLv3. http://gnu.org/licenses/gpl.txt")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("                          Press any key to continue...")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("             " & My.Application.Info.Copyright)
+        Console.WriteLine()
+        Console.WriteLine("################################################################################")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ShitBrick Tool/Starter.vb
@@ -0,0 +1,11 @@
+﻿Module Starter
+
+    Sub Main()
+        Console.WriteLine("Press any key to start the detection...")
+        Console.WriteLine()
+        Console.WriteLine("NOTICE: To fix the bug you will need administration rights.")
+        Console.ReadLine()
+        Detection.Main()
+    End Sub
+
+End Module
